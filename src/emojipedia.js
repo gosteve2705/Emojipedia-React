e32075b7b26c03b39e0aa9f13ac1c5336d0a55ed
Nomuoja Oghenemaro <38343496+gosteve2705@users.noreply.github.com>
2020-07-02 15:54:42 +0100

    added emoji

--- src/emojipedia.js
+++ src/emojipedia.js
@@ -31,6 +31,12 @@ const emojipedia = [
     emoji: "😃",
     name: "Smiling Face With Open Mouth",
     meaning:"Typical smiley face with open mouth and oval eyes. Is in a positive mood, shows its teeth and laughs cheerfully. Expresses enthusiasm: from a cheerful greeting to boundless joy"
+  },
+  {
+    id:6,
+    emoji: "😇",
+    name: "Smiling Face With Halo",
+    meaning:"Would like to express its (possibly pretended) innocence. Someone does good deeds, is very sweet, innocent and behaves in an exemplary way. The angelic face can also be used humorously for rather not so good deeds or behaviors"
   }
 ];
 
